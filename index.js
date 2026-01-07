09aa88bf92e7c5c578908654960cc4f747760050
Thomas Zerr <thomas.zerr@gmail.com>
2026-01-07 19:28:04 +0100

    Add camera functionality

--- index.js
+++ index.js
@@ -5,6 +5,34 @@ const FOREGROUND = "limegreen";
 const FPS = 60;
 const DELTA_TIME = 1 / FPS;
 
+const CAMERA_SPEED = 1;
+const CAMERA_POSITION = { x: 0, y: 0, z: 0 };
+
+const INPUTS = {
+  "ArrowUp": false,
+  "ArrowDown": false,
+  "ArrowLeft": false,
+  "ArrowRight": false,
+  "w": false,
+  "s": false,
+}
+
+//Setup input handling
+window.addEventListener("keydown", (keyEvent) => {
+  if (Object.keys(INPUTS).includes(keyEvent.key)) {
+    keyEvent.preventDefault();
+    INPUTS[keyEvent.key] = true;
+  }
+});
+
+window.addEventListener("keyup", (keyEvent) => {
+  if (Object.keys(INPUTS).includes(keyEvent.key)) {
+    keyEvent.preventDefault();
+    INPUTS[keyEvent.key] = false;
+  }
+});
+
+//Setup rendering loop
 window.addEventListener("load", () => {
   const canvas = document.getElementById("canvas");
   canvas.width = SCREEN_WIDTH;
@@ -28,17 +56,41 @@ window.addEventListener("load", () => {
   }
 
   function render() {
-    clearScreen();
-    angle += Math.PI * DELTA_TIME;
+    //Handle Input
+    const cameraMovementOffset = CAMERA_SPEED * DELTA_TIME;
+    if (INPUTS.ArrowUp) {
+      CAMERA_POSITION.z += cameraMovementOffset;
+    }
+    if (INPUTS.ArrowDown) {
+      CAMERA_POSITION.z -= cameraMovementOffset;
+    }
+    if (INPUTS.ArrowRight) {
+      CAMERA_POSITION.x += cameraMovementOffset;
+    }
+    if (INPUTS.ArrowLeft) {
+      CAMERA_POSITION.x -= cameraMovementOffset;
+    }
+    if (INPUTS.w) {
+      CAMERA_POSITION.y += cameraMovementOffset;
+    }
+    if (INPUTS.s) {
+      CAMERA_POSITION.y -= cameraMovementOffset;
+    }
 
+    //Render bodies
+    clearScreen();
+
+    angle += Math.PI * DELTA_TIME;
     for (const shape of bodies) {
       for (const vertice of shape.vertices) {
-        const start = projectToScreen(projectTo2d(translate(rotateAroundY(shape.points[vertice[0]], angle), shape.translation)));
-        const end = projectToScreen(projectTo2d(translate(rotateAroundY(shape.points[vertice[1]], angle), shape.translation)));
+        const cameraToShapeTranslation = translate(shape.translation, CAMERA_POSITION, -1);
+        const start = projectAndTranslate(rotateAroundY(shape.points[vertice[0]], angle), cameraToShapeTranslation);
+        const end = projectAndTranslate(rotateAroundY(shape.points[vertice[1]], angle), cameraToShapeTranslation);
         renderLine(start, end);
       }
     }
 
+    //Setup loop;
     clearTimeout(timeoutId);
     timeoutId = setTimeout(render, 1000 / FPS);
   }
